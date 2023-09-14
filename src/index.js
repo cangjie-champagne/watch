520f99762c77968d12edc73b17d284cf82233ac8
João Pedro <joaopjt@icloud.com>
2023-09-14 16:59:19 -0300

    added d() method values

--- src/index.js
+++ src/index.js
@@ -220,7 +220,7 @@ class Watch {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['mouth'],
       web: []
     };
     
@@ -236,19 +236,19 @@ class Watch {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['field'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['fire'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['predict'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['water'];
           break;
       }
     }
