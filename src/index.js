07687f97b91adc7acf01e36d0b5b83291834c360
João Pedro <joaopjt@gmail.com>
2023-09-15 16:53:43 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const letters = require('./letters.json');
-const letters_knife = require('../node_modules/telephone-booth/index.json');
+const letters_knife = require('telephone-booth');
 const cangjie = require('./cangjie.json');
 
 class Watch {
